681e29716103fcda16ab2aa5fef63b7c67b12e25
Martin <martingramajo08@gmail.com>
2024-11-08 12:43:09 -0300

    config iniciales

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <div className="text-center  text-green-800  ">
+        <h1 className="">
+        ¡Hola, Tailwind en React!
+
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
