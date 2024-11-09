d3bd67f889613d15b215dd7a3ee9221a0539c228
Martin G <martingramajo08@gmail.com>
2024-11-09 17:07:34 -0300

    agrego component about para trabajarlo

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,10 @@
+
+
+export * from 'about'
 export * from './AnimatedComponent'
 export * from './Hero';
+export * from './Loader'
+export * from './LoaderComputer'
 export * from './Navbar'
+export * from './Skills'
+export * from './Works'
