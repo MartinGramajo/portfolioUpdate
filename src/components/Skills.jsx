2f3f036eda5b33653585894cbc98c67208139218
Martin G <martingramajo08@gmail.com>
2024-11-09 16:48:58 -0300

    agrego modificaciones en ball y skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,6 +3,8 @@ import { motion } from "framer-motion";
 import { styles } from "../styles/styles";
 import { textVariant } from "../utils/motion";
 import { SectionWrapper } from "../hoc";
+import {technologies} from '../constants'
+import BallCanvas from "../canvas/Ball";
 
 const Skills = () => {
   return (
@@ -11,6 +13,13 @@ const Skills = () => {
         <p className={`${styles.sectionSubText} `}>Mis Skills</p>
         <h2 className={`${styles.sectionHeadText}`}>Tecnologías</h2>
       </motion.div>
+      <div className='flex flex-row flex-wrap justify-center gap-10 mt-10'>
+      {technologies.map((technology) => (
+        <div className='w-28 h-28' key={technology.name}>
+          <BallCanvas icon={technology.icon} />
+        </div>
+      ))}
+    </div>
     </>
   );
 };
