2c5f012e40a1f5fdca53958237a927050c599c16
Martin G <martingramajo08@gmail.com>
2024-11-10 21:19:11 -0300

    quito la animacion de entrada en works

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -17,7 +17,7 @@ const ProjectCard = ({
   source_code_link,
 }) => {
   return (
-    <motion.div variants={fadeIn("right", "spring", index * 0.5, 0.75)}>
+    <motion.div >
       <Tilt
         options={{
           max: 45,
@@ -70,7 +70,7 @@ const ProjectCard = ({
 const Works = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
+      <motion.div >
         <p className={`${styles.sectionSubText} `}>Mi trabajo</p>
         <h2 className={`${styles.sectionHeadText}`}>Proyectos.</h2>
       </motion.div>
