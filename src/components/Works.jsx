9d270464ce9e3203771ae417e5ab41099978b245
Martin G <martingramajo08@gmail.com>
2024-11-10 21:30:08 -0300

    agrego animacion en escritorio y dejo fijo en mobile en works

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -17,15 +17,58 @@ const ProjectCard = ({
   source_code_link,
 }) => {
   return (
-    <motion.div >
-      <Tilt
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className="bg-tertiary p-4 rounded-2xl  w-[350px] h-[480px]"
+    <>
+      {/* Animación solo en pantallas grandes */}
+      <motion.div
+        variants={fadeIn("up", "spring", index * 0.5, 0.75)}
+        className="hidden sm:block"
       >
+        <Tilt
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className="bg-tertiary p-4 rounded-2xl w-[350px] h-[480px]"
+        >
+          <div className="relative w-full h-[230px]">
+            <img
+              src={image}
+              alt="project_image"
+              className="w-full h-full object-cover rounded-2xl"
+            />
+
+            <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+              <div
+                onClick={() => window.open(source_code_link, "_blank")}
+                className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+              >
+                <img
+                  src={web}
+                  alt="source code"
+                  className="w-1/2 h-1/2 object-contain"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-5">
+            <h3 className="text-white font-bold text-[18px]">{name}</h3>
+            <p className="mt-2 text-secondary text-[14px]">{description}</p>
+          </div>
+
+          <div className="mt-4 flex flex-wrap gap-2">
+            {tags.map((tag) => (
+              <p key={`${name}-${tag.name}`} className={`text-[14px] ${tag.color}`}>
+                #{tag.name}
+              </p>
+            ))}
+          </div>
+        </Tilt>
+      </motion.div>
+
+      {/* Sin animación en dispositivos móviles */}
+      <div className="sm:hidden bg-tertiary p-4 rounded-2xl w-[350px] h-[480px]">
         <div className="relative w-full h-[230px]">
           <img
             src={image}
@@ -54,33 +97,43 @@ const ProjectCard = ({
 
         <div className="mt-4 flex flex-wrap gap-2">
           {tags.map((tag) => (
-            <p
-              key={`${name}-${tag.name}`}
-              className={`text-[14px] ${tag.color}`}
-            >
+            <p key={`${name}-${tag.name}`} className={`text-[14px] ${tag.color}`}>
               #{tag.name}
             </p>
           ))}
         </div>
-      </Tilt>
-    </motion.div>
+      </div>
+    </>
   );
 };
+
 
 const Works = () => {
   return (
     <>
-      <motion.div  variants={textVariant()} >
+      <motion.div variants={textVariant()} className="hidden sm:block">
+        <p className={`${styles.sectionSubText}`}>Mi trabajo</p>
+        <h2 className={`${styles.sectionHeadText}`}>Proyectos.</h2>
+      </motion.div>
+      <motion.div  className="block sm:hidden">
         <p className={`${styles.sectionSubText} `}>Mi trabajo</p>
         <h2 className={`${styles.sectionHeadText}`}>Proyectos.</h2>
       </motion.div>
 
-      <div className="w-full flex">
+      <div className="w-full flex hidden sm:block">
         <motion.p
           variants={fadeIn("", "", 0.1, 1)}
           className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
-             "Trabajemos juntos y creemos algo único"
+          "Trabajemos juntos y creemos algo único"
+        </motion.p>
+      </div>
+      <div className="w-full flex block sm:hidden">
+        <motion.p
+         
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          "Trabajemos juntos y creemos algo único"
         </motion.p>
       </div>
 
